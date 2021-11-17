80bd1280ebf09dff97d6b69a1458935da2d1273f
Nadine Alcantara <54692084+nadine-alcantara@users.noreply.github.com>
2021-11-16 14:45:29 -1000

    Created app and added data

---
+++ app/src/App.js
@@ -0,0 +1,36 @@
+import React from 'react';
+import Plot from 'react-plotly.js';
+import { Divider, Header } from 'semantic-ui-react';
+import './style/style.css'
+
+class App extends React.Component {
+  render() {
+    return (
+      <div>
+        <Header as="h1" textAlign="center" id="headerTitle">Covid Visualization</Header>
+        {/* sameple plot */}
+        <Plot
+          data={[
+            {
+              x: [1, 2, 3],
+              y: [2, 6, 3],
+              type: 'scatter',
+              mode: 'lines+markers',
+              marker: { color: 'red' },
+            },
+            { type: 'bar', x: [1, 2, 3], y: [2, 5, 3] },
+          ]}
+          layout={{ width: 320, height: 240, title: 'A Fancy Plot' }}
+        />
+        <Divider/>
+        <div id="footer">
+          <p>App created by Nadine Alcantara, Aaron Kam, and Irene Ma</p>
+          <p>Visualized using <u><a href="https://plotly.com/javascript/react/">Plotly.js</a></u></p>
+        </div>
+        <br />
+      </div>
+    );
+  }
+}
+
+export default App;
