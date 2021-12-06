a6a6f953eebbc30cab2197207142e56b281a4058
Nadine Alcantara <54692084+nadine-alcantara@users.noreply.github.com>
2021-12-05 15:46:33 -1000

    Formatted charts into cards

--- app/src/App.js
+++ app/src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Divider, Header } from 'semantic-ui-react';
+import { Card, Divider, Header } from 'semantic-ui-react';
 import './style/style.css';
 import RaceAndEthnicity from './RaceAndEthnicity';
 import VaccinUS from './VaccinUS';
@@ -16,15 +16,44 @@ class App extends React.Component {
         <Header as="h1" textAlign="center" id="headerTitle">
           Likelihood of Your Death by Covid
         </Header>
+        <Card.Group>
+          <Card fluid id="landing">
+            <Card.Content id="chart-display">
+              <RaceAndEthnicity />
+            </Card.Content>
+          </Card>
 
-        <RaceAndEthnicity />        
-        <Sex />
-        <SexPieCharts />
-        <AgeGroups />
-        <VaccinUS />
+          <Card fluid id="landing">
+            <Card.Content id="chart-display">
+              <Sex />
+            </Card.Content>
+          </Card>
+
+          <Card fluid id="landing">
+            <Card.Content id="chart-display">
+              <SexPieCharts />
+            </Card.Content>
+          </Card>
+
+          <Card fluid id="landing">
+            <Card.Content id="chart-display">
+              <AgeGroups />
+            </Card.Content>
+          </Card>
+
+          <Card fluid id="landing">
+            <Card.Content id="chart-display">
+              <VaccinUS />
+            </Card.Content>
+          </Card>
+
+          <Card fluid id="landing">
+            <Card.Content id="chart-display">
+              <RiskDashboard />
+            </Card.Content>
+          </Card>
+        </Card.Group>
         <Divider />
-        <RiskDashboard />
-
         <div id="footer">
           <p>App created by Nadine Alcantara, Aaron Kam, and Irene Ma</p>
           <p>Visualized using <u><a href="https://plotly.com/javascript/react/">Plotly.js</a></u></p>
